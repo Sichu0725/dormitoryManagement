542940ad461ef1fa011b3eb56bd082e9703bef0a
Sichu0725 <choihc8511@gmail.com>
2023-03-06 16:00:13 +0900

    feat:project initialize

---
+++ dormitoryManagementServer/src/main/java/com/gbsw/dormitory/management/dormitorymanagementserver/DormitoryManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.gbsw.dormitory.management.dormitorymanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitoryManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormitoryManagementServerApplication.class, args);
+	}
+
+}
---
+++ dormitoryManagementServer/src/test/java/com/gbsw/dormitory/management/dormitorymanagementserver/DormitoryManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbsw.dormitory.management.dormitorymanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitoryManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
