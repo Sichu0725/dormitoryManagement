e1aa46fa88a2a2b0115a3ff9f1877ea5d3e551e9
Sichu0725 <choihc8511@gmail.com>
2023-03-07 13:52:30 +0900

    feat:add mobile nfc code

--- dormitory_management_mobile/app/src/main/java/com/gbsw/hs/kr/dormitory_management_mobile/MainActivity.java
+++ dormitory_management_mobile/app/src/main/java/com/gbsw/hs/kr/dormitory_management_mobile/MainActivity.java
@@ -1,14 +1,36 @@
 package com.gbsw.hs.kr.dormitory_management_mobile;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView _tv;
+    private Intent _cardService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _tv = (TextView) findViewById(R.id._textView);
+
+        _cardService = new Intent(this, CardService.class);
+        startService(_cardService);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(_cardService);
+    }
+
+    public void onClickUpdate(View v) {
+        _tv.setText("Count : " + Counter.GetCurrentCout());
     }
 }
